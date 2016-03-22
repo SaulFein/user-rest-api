4f0a20c94e0b5604aa8746834fa29c747aa23ddf
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-03-22 13:14:59 -0700

    created basic files and got tokens working

---
+++ routes/new-user.js
@@ -0,0 +1,17 @@
+'use strict'
+let jwt = require('jsonwebtoken')
+let User = require('../models/user')
+
+module.exports = (router) => {
+  router.post('/new-user', (req, res) => {
+    // res.json(req.body);
+    var newUser = new User(req.body);
+    console.log(newUser);
+    newUser.save((err, user) => {
+      res.json(user);
+    })
+  })
+}
+
+
+//curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"123"}' http://localhost:3000/new-user
---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+let mongoose = require('mongoose')
+
+let publicRouter = express.Router()
+let apiRouter = express.Router()
+
+let DB_PORT = 'mongodb://localhost/db'
+mongoose.connect(DB_PORT);
+
+require('./routes/login')(publicRouter) //dependency injection
+require('./routes/new-user')(publicRouter)
+
+app.use(bodyParser.json())
+app.use('/public', publicRouter)
+//app.use('/api', require('./lib/authenticate'))
+//app.use('/api', apiRouter)
+
+app.listen(3000, () => {
+  console.log('server up');
+})
+
+//curl -X POST -H 'Authorization: basic name:password' http://localhost:3000/public/login
+
+//curl -X POST -u myusername:mypassword http://localhost:3000/public/login
